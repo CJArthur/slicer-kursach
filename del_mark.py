089a5092c39ee6793e9fdae2d55085cadc99a9ed
CJArthur <arthur-nahushev@mail.ru>
2026-03-25 23:47:34 +0300

    New overlay

--- del_mark.py
+++ del_mark.py
@@ -2,9 +2,11 @@ import subprocess
 
 #make schema of frame
 # ffmpeg -i input.mp4 -vf "drawgrid=width=100:height=100:color=red@0.3" -frames:v 1 test.png
+video_name = "Pumpkin Spice Latte Lover's MELTDOWN!"
+
 subprocess.run(["ffmpeg",
                 "-i",
-                "Police Interrogation_ The Lois Case - Smoking Gun Found!.mp4",
+                f"{video_name}.mp4",
                 "-vf",
                 "drawgrid=width=100:height=100:color=red@0.3",
                 "-frames:v",
@@ -14,13 +16,13 @@ subprocess.run(["ffmpeg",
 # make overlay video
 subprocess.run(["ffmpeg",
                 "-i",
-                "Police Interrogation_ The Lois Case - Smoking Gun Found!.mp4",
+                f"{video_name}.mp4",
                 "-stream_loop",
                 "-1",
                 "-i",
                 "overlay.mp4",
                 "-filter_complex",
-                "[1:v]scale=900:650[ovr];[0:v][ovr]overlay=(main_w-overlay_w)/2:5",
+                "[1:v]scale=900:600[ovr];[0:v][ovr]overlay=(main_w-overlay_w)/2:47",
                 "-shortest",
                 "output.mp4"])
 
@@ -30,5 +32,5 @@ subprocess.run(["ffmpeg",
                 "-i", 
                 "output.mp4", 
                 "-vf", 
-                "delogo=x=70:y=654:w=150:h=46:enable='between(t,4,7.3)'",
-                "output_new.mp4"])
+                "delogo=x=75:y=646:w=140:h=55:enable='between(t,4,7.3)'",
+                f"{video_name}_new.mp4"])
